af92e496e2f6e498cade51fb92e9c57e576a210a
mucheru-delvan <mkdelvan9@gmail.com>
2026-09-10 09:14:53 +0300

    Refactored the accounts.models.py file

--- accounts/models.py
+++ accounts/models.py
@@ -14,10 +14,7 @@ class Account(models.Model):
         on_delete=models.CASCADE,
         related_name="accounts",
     )
-#user means that the account is linked to a specific user in the system. 
-# The ForeignKey establishes a many-to-one relationship, meaning that each account belongs to one user, but a user can have multiple accounts. 
-# The on_delete=models.CASCADE argument specifies that if the user is deleted, all associated accounts will also be deleted. 
-# The related_name="accounts" allows you to access a user's accounts using user.accounts.
+
     status = models.CharField(
         max_length=10,
         choices=AccountStatus.choices,
@@ -33,13 +30,6 @@ class Account(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-        #This tells Django to create a database index on the combination of the user and status fields.
-        #Think of an index like the index at the back of a textbook.
-        #without an index, if you wanted to find all accounts for a specific user with a specific status, 
-        # the database would have to look through every account record one by one until it found the right ones.
-        #With an index, the database can quickly locate the relevant records,
-        # much like how you can quickly find a topic in a textbook by looking it up in the index.
-        #This can significantly speed up queries that filter on these fields, especially in a large database.
         indexes = [
             models.Index(fields=["user", "status"]),
         ]
